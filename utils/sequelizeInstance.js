a44a86c08b2eeb4affb32bcd1546450b4a576217
adil2190 <madil219076@gmail.com>
2025-08-07 15:49:30 +0500

    more settings added

--- utils/sequelizeInstance.js
+++ utils/sequelizeInstance.js
@@ -14,6 +14,11 @@ const createSequelizeInstance = (databaseName) => {
       dialectOptions: {
         connectTimeout: 1000000,
       },
+      pool: {
+        max: 100,
+        min: 0,
+        idle: 1000000,
+      },
     }
   );
   console.log(sequelize.config.database, "active database");
