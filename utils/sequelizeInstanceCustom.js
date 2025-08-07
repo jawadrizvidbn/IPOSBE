f668e0c23e9cebd3ecf86b0495227d834398aaed
adil2190 <madil219076@gmail.com>
2025-08-07 15:58:15 +0500

    again timeout

--- utils/sequelizeInstanceCustom.js
+++ utils/sequelizeInstanceCustom.js
@@ -7,7 +7,7 @@ const createSequelizeInstanceCustom = ({
   username,
   password,
   host,
-  port = "3306"
+  port = "3306",
 }) => {
   console.log({ databaseName, username, password, host });
   const sequelize = new Sequelize(databaseName, username, password, {
@@ -15,6 +15,14 @@ const createSequelizeInstanceCustom = ({
     dialect: "mysql", // Explicitly specify the dialect,
     port: port,
     logging: false,
+    dialectOptions: {
+      connectTimeout: 1000000,
+    },
+    pool: {
+      max: 100,
+      min: 0,
+      idle: 1000000,
+    },
   });
   console.log(sequelize.config.database, "active database");
   return sequelize;
